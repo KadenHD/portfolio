ac1695ed34f84c9dfc8b4153c03e81ab919ec8b6
KadenHD <pierraored@gmail.com>
2026-01-19 11:48:15 +0100

    convert to french + add calendly

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import './App.css'
+import CalendlyPopup from './components/CalendlyPopup'
 
 function App() {
   return (
@@ -12,18 +13,20 @@ function App() {
 
       <div className="content">
         <h1 className="typing">
-          Hi, I'm Pierre CLÉMENT<span className="cursor">|</span>
+          Pierre CLÉMENT
+          <span className="cursor">|</span>
         </h1>
         <h2>
-          Portfolio Under Construction
+          Portfolio En Travaux
         </h2>
         <div className="spinner"></div>
-        <p>I'm currently working on projects (including this website).</p>
-        <p>Check back soon or contact me!</p>
+        <p>Je travail actuellement sur des projets (dont ce site).</p>
+        <p>Revenez plus tard ou contactez moi !</p>
         <div className="socials">
           <a href="https://github.com/KadenHD" target="_blank">GitHub</a>
           <a href="https://linkedin.com/in/pierre--clement" target="_blank">LinkedIn</a>
           <a href="mailto:pierre.clement.oise@gmail.com">Email</a>
+          <CalendlyPopup/>
         </div>
       </div>
     </div>
